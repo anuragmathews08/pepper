c9f588460cab362612c8bb2093060a93acb40024
anuragmathews08 <anuragmathews619@gmail.com>
2021-01-15 10:24:00 +0530

    final dropdown creation commit

--- src/container/Home/Home.js
+++ src/container/Home/Home.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import classes from './Home.module.css';
 
 function Home() {
     return (
-        <div>
+        <div className={classes.homeBlock}>
             <h1>This is Home page!</h1>
         </div>
     )
